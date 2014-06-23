67a00c77a6b2f059225c8f03fbc6da5b143c5eac
Charles Lowell <cowboyd@frontside.io>
2014-06-22 22:50:52 -0500

    expose integer values

--- ext/beguiler/native.c
+++ ext/beguiler/native.c
@@ -1,5 +1,20 @@
 #include <libguile.h>
+#include <ruby.h>
+
+
+VALUE bg_scm_c_eval_string(VALUE self, VALUE string) {
+  const char * ptr = RSTRING_PTR(StringValue(string));
+  SCM result = scm_c_eval_string(ptr);
+  if (scm_is_integer(result)) {
+    return INT2FIX(scm_to_int(result));
+  }
+  return Qnil;
+}
 
 void Init_native() {
-  
+  scm_init_guile();
+  VALUE Beguiler = rb_define_module("Beguiler");
+  VALUE C = rb_define_module_under(Beguiler, "C");
+
+  rb_define_singleton_method(C, "scm_c_eval_string", bg_scm_c_eval_string, 1);
 }
--- lib/beguiler/context.rb
+++ lib/beguiler/context.rb
@@ -1,7 +1,7 @@
 module Beguiler
   class Context
-    def evaluate(code)
-      
+    def evaluate(string)
+      C.scm_c_eval_string string
     end
   end
 end
